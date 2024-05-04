65267afa1cb6ab82b5a38a8d150a540327dd90d0
Veng-Eang <oengvengeang@gmail.com>
2024-05-04 10:59:05 -0500

    route manager

---
+++ lib/presentation/resources/routes_manager.dart
@@ -0,0 +1,8 @@
+class Routes {
+  static const String splashRoute = "/";
+  static const String onBoardingRoute = "/onBoarding";
+  static const String loginRoute = "/login";
+  static const String fogotPasswordRoute = "/forgotPassword";
+  static const String homeRoute = "/home";
+  static const String storeDetailsRoute = "/storeDeatials";
+}
--- lib/presentation/resources/style_manager.dart
+++ lib/presentation/resources/style_manager.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_advance/presentation/font_manager.dart';
+import 'package:flutter_advance/presentation/resources/font_manager.dart';
 
 TextStyle _getTextStyle(
   double fontSize,
