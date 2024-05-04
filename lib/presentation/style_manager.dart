147b2bad6d59147f3c9a62d09bbf1eeb3b29a91f
Veng-Eang <oengvengeang@gmail.com>
2024-05-04 07:32:14 -0500

    style manager

---
+++ lib/presentation/style_manager.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_advance/presentation/font_manager.dart';
+
+TextStyle _getTextStyle(
+  double fontSize,
+  String fontFamily,
+  FontWeight fontWeight,
+  Color color,
+) {
+  return TextStyle(
+      fontSize: fontSize,
+      fontFamily: fontFamily,
+      color: color,
+      fontWeight: fontWeight);
+}
+
+// regular style
+TextStyle getRegularStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+      fontSize, FontContants.fontFamily, FontWeightManager.regular, color);
+}
+
+// light text style
+TextStyle getLightStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+      fontSize, FontContants.fontFamily, FontWeightManager.light, color);
+}
+
+// bold text style
+TextStyle getBoldStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+      fontSize, FontContants.fontFamily, FontWeightManager.bold, color);
+}
+
+// semi bold text style
+TextStyle getSemiBoldStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+      fontSize, FontContants.fontFamily, FontWeightManager.semiBold, color);
+}
+
+//medium text style
+TextStyle getMediumStyle({
+  double fontSize = FontSize.s12,
+  required Color color,
+}) {
+  return _getTextStyle(
+      fontSize, FontContants.fontFamily, FontWeightManager.medium, color);
+}
